ea642c2022133bbe70513b84aaca8ff58b45ad07
mun <quangnd.edu@gmail.com>
2019-04-25 09:32:35 +0700

    Add entrypoint for vendor js file, add bootstrap js
    - Now we have 2 entry files and 2 bundles
    - The vendor file houses code that is less likely to change, 3rd party libraries
    - The main file has all of our app code

--- webpack.common.js
+++ webpack.common.js
@@ -3,10 +3,13 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
   output: {
     filename: "main.[contentHash].js",
     path: path.resolve(__dirname, "dist")
+  },
+  entry: {
+    main: "./src/index.js",
+    vendor: "./src/vendor.js"
   },
   plugins: [
     new HtmlWebpackPlugin({
--- webpack.prod.js
+++ webpack.prod.js
@@ -5,9 +5,8 @@ const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "development",
-  entry: "./src/index.js",
   output: {
-    filename: "main.[contentHash].js",
+    filename: "[name].[contentHash].bundle.js",
     path: path.resolve(__dirname, "dist")
   },
   plugins: [
